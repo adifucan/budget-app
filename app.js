373d44f80b9c9cd5c8ec7fc965f82f6099693e88
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-08-29 23:56:32 -0500

    Setting up the First Event Listeners

--- app.js
+++ app.js
@@ -10,5 +10,7 @@ var UIController = (function () {
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-
+    document.querySelector('.add__btn').addEventListener('click', function () {
+        console.log('Button was clicked.')
+    });
 })(budgetController, UIController);
