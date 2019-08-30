beaff7e6cc5ed86008ae851eb01e2280d6a0657b
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-08-30 00:20:04 -0500

    Setting up the First Event Listeners

--- app.js
+++ app.js
@@ -10,7 +10,25 @@ var UIController = (function () {
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-    document.querySelector('.add__btn').addEventListener('click', function () {
-        console.log('Button was clicked.')
+    var controllAddItem = function () {
+        // 1. get field input data
+
+        // 2. Add item to the budget controller
+
+        // 3. Add item to the UI
+
+        // 4. Calculate budget
+
+        // 5. Display budget on the UI
+
+        console.log('It works');
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', controllAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter' || event.code === 'Enter') {
+            controllAddItem();
+        }
     });
 })(budgetController, UIController);
