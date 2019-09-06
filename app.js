1cacbe11d6f66036416229bcdb4462e65f23739a
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-09-05 23:40:55 -0500

    Reading Input Data

--- app.js
+++ app.js
@@ -6,12 +6,39 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            }
+        },
+
+        getDomStrings: function () {
+            return DOMStrings;
+        }
+    }
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDomStrings();
+
     var controllAddItem = function () {
+
         // 1. get field input data
+        const input = UICtrl.getInput();
+
+        console.log(input);
 
         // 2. Add item to the budget controller
 
@@ -20,11 +47,9 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 4. Calculate budget
 
         // 5. Display budget on the UI
-
-        console.log('It works');
     };
 
-    document.querySelector('.add__btn').addEventListener('click', controllAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', controllAddItem);
 
     document.addEventListener('keypress', function (event) {
         if (event.key === 'Enter' || event.code === 'Enter') {
