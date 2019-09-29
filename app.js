2ebf62f877364fc5ec2614d8e6901ab757003b3a
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-09-29 15:38:38 -0500

    Deleting an Item from Our Budget Controller

--- app.js
+++ app.js
@@ -57,6 +57,22 @@ var budgetController = (function() {
             return newItem;
         },
 
+        deleteItem: function(type, id) {
+            var ids, index;
+
+            // create new array of ids to find the index of the id we need to delete
+            // as original array of all items can be like [1, 3, 5, 7] with already deleted items
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculateBudget() {
 
             // calculate total income and expenses
@@ -252,9 +268,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         if (itemID) {
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. Delete item from data structure
+            budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete item from UI
 
